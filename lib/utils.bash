aa0aee3d746d379e1e09cf61f4582cd2b757fc51
Jacob Gillespie <jacobwgillespie@gmail.com>
2023-10-01 21:22:39 +0100

    Update formatting

--- lib/utils.bash
+++ lib/utils.bash
@@ -72,11 +72,11 @@ install_version() {
 # Implementation taken from
 # https://stackoverflow.com/a/4024263
 verlte() {
-    [  "$1" = "`echo -e "$1\n$2" | sort -V | head -n1`" ]
+  [  "$1" = "`echo -e "$1\n$2" | sort -V | head -n1`" ]
 }
 
 verlt() {
-    [ "$1" = "$2" ] && return 1 || verlte $1 $2
+  [ "$1" = "$2" ] && return 1 || verlte $1 $2
 }
 
 get_tarball_name() {
